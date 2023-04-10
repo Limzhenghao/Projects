f716f28eff159dcb1177514717ca5b4bda7d5639
Lim Zheng Hao <85433320+Limzhenghao@users.noreply.github.com>
2023-04-10 21:16:10 +0800

    Delete tic-tac-toe-game directory

--- tic-tac-toe-game/game-logic.js
+++
@@ -1,9 +0,0 @@
-const editButton = document.getElementById('edit-button1');
-const inputOverlay1 = document.querySelector('.input-player1');
-const inputBackdrop = document.getElementById('backdrop');
-function showInputOverlay1() {
-    inputOverlay1.style.display = 'block';
-    inputBackdrop.style.display = 'block';
-}
-
-editButton.addEventListener('click', showInputOverlay1);
